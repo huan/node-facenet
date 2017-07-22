0bdcea31424e339f807f290b0f6522532b0ced0f
Huan LI <zixia@zixia.net>
2017-07-22 11:26:26 +0800

    working...

--- src/python-facenet.ts
+++ src/python-facenet.ts
@@ -1,16 +1,19 @@
-const pythonBridge = require('python-bridge')
+import { pythonBridge } from 'python-bridge'
 
 export class PythonFacenet {
   private python: any
 
   constructor() {
+    //
+  }
+
+  public async init(): Promise<void> {
     this.python = pythonBridge({
       env: {
-        PYTHONPATH: '/python-facenet/src/',
+        PYTHONPATH: `${__dirname}/../../python-facenet/src/`,
       },
     })
   }
-
   public async test1() {
     return await this.python`1+1`
   }
