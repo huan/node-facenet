10dad16f2340989236b3b400fb1526385663ada8
Huan LI <zixia@zixia.net>
2017-07-23 11:49:02 +0800

    work

--- src/facenet.ts
+++ src/facenet.ts
@@ -1,24 +1,62 @@
-import * as ndarray from 'ndarray'
-const nj = require('numjs')
+import { promisify } from 'util'
 
-export type FeatureVector = number[]  // 128 dim
+import * as ndarray from 'ndarray'
+
+const distance  = require('ndarray-distance')
+const getPixels = require('get-pixels')
+// const nj        = require('numjs')
+
+import { PythonFacenet } from './python-facenet'
+
+export type FeatureVector = ndarray  // 128 dim
+export type ImageVector   = ndarray
+
+export interface FaceRect {
+  top:    number,
+  left:   number,
+  width:  number,
+  height: number,
+}
 
 export class Facenet {
+  private pythonFacenet: PythonFacenet
 
   constructor() {
-    // TODO
+    //
+  }
+
+  public async init(): Promise<void> {
+    this.pythonFacenet = new PythonFacenet()
+    await this.pythonFacenet.init()
+  }
+
+  public async image(url: string): Promise<ImageVector> {
+    return await promisify(getPixels)(url)
+  }
+
+  public async scan(image: ImageVector): Promise<FaceRect[]> {
+    const top     = 0
+    const left    = 0
+    const height  = image.shape[0]
+    const width   = image.shape[1]
+    return [{
+      top,
+      left,
+      width,
+      height,
+    }]
   }
 
   /**
    * Get the 128 dims embeding from image(s)
    */
-  public embeding(image: ndarray): FeatureVector[] {
+  public async embeding(image: ImageVector): Promise<FeatureVector[]> {
     console.log(image)
     // TODO
-    return [[1, 2, 3]]
+    return [ndarray([1, 2, 3])]
   }
 
   public distance(embedding1: FeatureVector, embedding2: FeatureVector): number {
-    return nj.linalg.distance(embedding1, embedding2)
+    return distance(embedding1, embedding2)
   }
 }
