83050b32a981680f6ca8e8a3b4d89e7acf664c13
Huan LI <zixia@zixia.net>
2017-08-04 00:14:44 +0800

    check-version for install

---
+++ bin/check-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+python::version() {
+  echo $(python3 -c 'import sys; print(sys.version_info)')
+  ret=`python3 -c 'import sys; v = sys.version_info; print("%i" % (v.major <3 or v.major==3 and v.minor<5));'`
+  if [ $ret != 0 ]; then
+    echo "Python version chec failed: minimum requirement is v3.5 (3.6 or above is recommended)"
+    exit 1
+  fi
+}
+
+main() {
+  python::version
+}
+
+main "$@"
