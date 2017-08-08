34436ef89b73785a3ad79973921cf1246c827c56
Huan LI <zixia@zixia.net>
2017-08-08 19:37:29 +0800

    code clean

--- src/dataset.ts
+++ src/dataset.ts
@@ -12,7 +12,6 @@ export interface IdImageList {
 
 export abstract class Dataset {
   // all is relative path
-  private idListCache:      string[]
   private imageListCache:   string[]
   private idImageListCache: IdImageList
 
@@ -31,20 +30,8 @@ export abstract class Dataset {
   public async idList(): Promise<string[]> {
     log.verbose('Dataset', 'idList()')
 
-    if (this.idListCache) {
-      return this.idListCache
-    }
-
-    return new Promise<string[]>((resolve, reject) => {
-      glob(`${this.directory}/*/`, (err, matches) => {
-        if (err) {
-          return reject(err)
-        }
-        this.idListCache = matches.map(match => path.relative(this.directory, match))
-        log.verbose('Dataset', 'idList() loaded')
-        resolve(this.idListCache)
-      })
-    })
+    const data = await this.idImageList()
+    return Object.keys(data)
   }
 
   /**
@@ -77,26 +64,22 @@ export abstract class Dataset {
     }
     this.idImageListCache = {}
 
-    return new Promise<IdImageList>((resolve, reject) => {
-      glob(`${this.directory}/**/*.${this.ext}`, (err, matches) => {
-        if (err) {
-          return reject(err)
-        }
-        matches.forEach(match => {
-          const parts = match.split(path.sep)
-          const id    = parts.slice(-2, -1)[0]
-          const image = parts.slice(-1)[0]
-          if (Array.isArray(this.idImageListCache[id])) {
-            this.idImageListCache[id].push(image)
-          } else {
-            this.idImageListCache[id] = [image]
-          }
-        })
-        log.verbose('Dataset', 'idImageListCache() loaded %d ids',
-                                Object.keys(this.idImageListCache).length,
-                  )
-        resolve(this.idImageListCache)
-      })
+    const imageList = await this.imageList()
+
+    imageList.forEach(imagePath => {
+      const parts = imagePath.split(path.sep)
+      const id    = parts.slice(-2, -1)[0]
+      const image = parts.slice(-1)[0]
+      if (Array.isArray(this.idImageListCache[id])) {
+        this.idImageListCache[id].push(image)
+      } else {
+        this.idImageListCache[id] = [image]
+      }
     })
+
+    log.verbose('Dataset', 'idImageListCache() loaded %d ids',
+                            Object.keys(this.idImageListCache).length,
+              )
+    return this.idImageListCache
   }
 }
