4dbe8a319bb81b1c2df82a20189f5fb830371d13
Huan LI <zixia@zixia.net>
2017-08-08 18:51:05 +0800

    dataset & unit tests

---
+++ src/dataset.spec.ts
@@ -0,0 +1,58 @@
+#!/usr/bin/env ts-node
+
+const t = require('tap')  // tslint:disable:no-shadowed-variable
+
+// import { log }      from './config'
+// log.level('silly')
+
+import { Dataset }  from './dataset'
+
+const DIRECTORY = `${__dirname}/../tests/fixtures/dataset`
+
+
+class DatasetTest extends Dataset {
+  public async setup(): Promise<void> {
+    //
+  }
+}
+
+t.test('Smoke testing', async (t: any) => {
+  const d = new DatasetTest(DIRECTORY, 'jpg')
+  t.ok(d, 'should inited a Dataset instance')
+})
+
+t.test('idList()', async (t: any) => {
+  const d = new DatasetTest(DIRECTORY, 'jpg')
+  const idList = await d.idList()
+
+  t.equal(idList.length, 2, 'should get 2 ids')
+  t.true(idList.includes('id1'), 'should contains id1')
+  t.true(idList.includes('id2'), 'should contains id2')
+})
+
+t.test('imageList()', async (t: any) => {
+  const d = new DatasetTest(DIRECTORY, 'jpg')
+  const imageList = await d.imageList()
+
+  t.equal(imageList.length, 3, 'should get 3 images')
+  t.true(imageList.includes('id1/image1.jpg'), 'should contains image1.jpg')
+  t.true(imageList.includes('id2/image2.jpg'), 'should contains image2.jpg')
+  t.true(imageList.includes('id2/image22.jpg'), 'should contains image22.jpg')
+})
+
+t.test('idImageList()', async (t: any) => {
+  const d = new DatasetTest(DIRECTORY, 'jpg')
+  const idImageList = await d.idImageList()
+
+  const idList = Object.keys(idImageList)
+  t.equal(idList.length, 2, 'should get 2 ids')
+  t.true(idList.includes('id1'), 'should contains id1')
+  t.true(idList.includes('id2'), 'should contains id2')
+
+  t.equal(idImageList['id1'].length, 1, 'should get 1 images for id1')
+  t.true(idImageList['id1'].includes('image1.jpg'), 'should contains image1.jpg')
+
+  t.equal(idImageList['id2'].length, 2, 'should get 2 images for id2')
+  t.true(idImageList['id2'].includes('image2.jpg'), 'should contains image2.jpg')
+  t.true(idImageList['id2'].includes('image22.jpg'), 'should contains image22.jpg')
+})
--- src/dataset.ts
+++ src/dataset.ts
@@ -2,25 +2,49 @@ import * as path  from 'path'
 
 import * as glob  from 'glob'
 
-import { log }    from './config'
+import {
+  log,
+}                         from './config'
 
 export interface IdImageList {
-  [id: string]: string[],
+  [id: string]: string[], // relative path
 }
 
-export class Dataset {
-  private idListCache: string[]
-  private imageListCache: string[]
+export abstract class Dataset {
+  // all is relative path
+  private idListCache:      string[]
+  private imageListCache:   string[]
   private idImageListCache: IdImageList
 
   constructor(
     public directory: string,
-    private ext: string = 'jpg',
+    public ext = 'jpg',
   ) {
-    log.verbose('Dataset', 'constructor(%s, %s)',
+    log.verbose('Dataset', 'constructor(directory=%s, ext=%s)',
                             directory,
                             ext,
               )
+  }
+
+  public abstract async setup(): Promise<void>  // Should be used to download/extract/initialize dataset files
+
+  public async idList(): Promise<string[]> {
+    log.verbose('Dataset', 'idList()')
+
+    if (this.idListCache) {
+      return this.idListCache
+    }
+
+    return new Promise<string[]>((resolve, reject) => {
+      glob(`${this.directory}/*/`, (err, matches) => {
+        if (err) {
+          return reject(err)
+        }
+        this.idListCache = matches.map(match => path.relative(this.directory, match))
+        log.verbose('Dataset', 'idList() loaded')
+        resolve(this.idListCache)
+      })
+    })
   }
 
   /**
@@ -45,23 +69,34 @@ export class Dataset {
     })
   }
 
-  public async idList(): Promise<string[]> {
-    log.verbose('Dataset', 'idList()')
-
-    if (this.idListCache) {
-      return this.idListCache
-    }
-    return [] // TODO
-  }
-
   public async idImageList(): Promise<IdImageList> {
     log.verbose('Dataset', 'idImageList()')
 
-    if (this.idImageListCache) {
+    if (this.idImageListCache && Object.keys(this.idImageListCache).length) {
       return this.idImageListCache
     }
-    return {
-      'x': [],
-    } // TODO
+    this.idImageListCache = {}
+
+    return new Promise<IdImageList>((resolve, reject) => {
+      glob(`${this.directory}/**/*.${this.ext}`, (err, matches) => {
+        if (err) {
+          return reject(err)
+        }
+        matches.forEach(match => {
+          const parts = match.split(path.sep)
+          const id    = parts.slice(-2, -1)[0]
+          const image = parts.slice(-1)[0]
+          if (Array.isArray(this.idImageListCache[id])) {
+            this.idImageListCache[id].push(image)
+          } else {
+            this.idImageListCache[id] = [image]
+          }
+        })
+        log.verbose('Dataset', 'idImageListCache() loaded %d ids',
+                                Object.keys(this.idImageListCache).length,
+                  )
+        resolve(this.idImageListCache)
+      })
+    })
   }
 }
