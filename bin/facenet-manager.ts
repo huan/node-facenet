2a12be51dc2361d10f1d172f28465a435c9774d5
Huan LI <zixia@zixia.net>
2017-09-07 00:31:36 +0800

    add warning

--- bin/facenet-manager.ts
+++ bin/facenet-manager.ts
@@ -120,6 +120,12 @@ function parseArguments(): Args {
   return parser.parseArgs()
 }
 
+process.on('warning', (warning) => {
+  console.warn(warning.name);    // Print the warning name
+  console.warn(warning.message); // Print the warning message
+  console.warn(warning.stack);   // Print the stack trace
+});
+
 log.level('silly')
 
 main(parseArguments())
