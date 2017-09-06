1189d59cb3cf13486a4581bf7a8f23f75dda8d21
Huan LI <zixia@zixia.net>
2017-09-06 11:25:13 +0800

    export db-cache for convenience

--- index.ts
+++ index.ts
@@ -8,13 +8,13 @@ export {
   Face,
 }                           from './src/face'
 export { Facenet }          from './src/facenet'
-// export *                    from './src/misc'
+export *                    from './src/misc'
 
 export { AlignmentCache }   from './src/cache/alignment-cache'
+export { DbCache }          from './src/cache/db-cache'
 export { EmbeddingCache }   from './src/cache/embedding-cache'
 
 export { Dataset }          from './src/dataset/'
 export { Lfw }              from './src/dataset/lfw'
 
 // export { PythonFacenet }    from './src/python3/python-facenet'
-export *                    from './src/misc'
