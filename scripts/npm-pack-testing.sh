1b33f83dc808f09fefa02be82776c0833c76d320
Huan LI <zixia@zixia.net>
2018-05-20 00:45:37 +0800

    enable CI/CD to NPM@next

---
+++ scripts/npm-pack-testing.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+
+npm run dist
+npm pack
+
+TMPDIR="/tmp/npm-pack-testing.$$"
+mkdir "$TMPDIR"
+mv *-*.*.*.tgz "$TMPDIR"
+cp tests/fixtures/smoke-testing.ts "$TMPDIR"
+
+cd $TMPDIR
+npm init -y
+npm install *-*.*.*.tgz \
+  typescript@latest
+
+./node_modules/.bin/tsc \
+  --lib esnext,dom \
+  --noEmitOnError \
+  --noImplicitAny \
+  --skipLibCheck \
+  smoke-testing.ts
+
+node smoke-testing.js
